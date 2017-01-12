7d2f1c55d60bdc886a3f5c7b7a3a401b54900a66
Sam Murray <samueljmurray@gmail.com>
2017-01-12 12:55:10 +0000

    grim workaround until https://github.com/speedskater/babel-plugin-rewire/issues/78 is resolved

--- definitions/index.js
+++ definitions/index.js
@@ -1,11 +1,149 @@
-export * from './attention-seekers';
-export * from './bouncing-entrances';
-export * from './bouncing-exits';
-export * from './fading-entrances';
-export * from './fading-exits';
-export * from './flippers';
-export * from './lightspeed';
-export * from './sliding-entrances';
-export * from './sliding-exits';
-export * from './zooming-entrances';
-export * from './zooming-exits';
+import {
+  bounce,
+  flash,
+  jello,
+  pulse,
+  rotate,
+  shake,
+  swing,
+  rubberBand,
+  tada,
+  wobble
+} from './attention-seekers';
+import {
+  bounceIn,
+  bounceInUp,
+  bounceInDown,
+  bounceInRight,
+  bounceInLeft
+} from './bouncing-entrances';
+import {
+  bounceOut,
+  bounceOutUp,
+  bounceOutDown,
+  bounceOutRight,
+  bounceOutLeft
+} from './bouncing-exits';
+import {
+  fadeIn,
+  fadeInDown,
+  fadeInUp,
+  fadeInLeft,
+  fadeInRight,
+  fadeInDownBig,
+  fadeInUpBig,
+  fadeInLeftBig,
+  fadeInRightBig
+} from './fading-entrances';
+import {
+  fadeOut,
+  fadeOutDown,
+  fadeOutUp,
+  fadeOutLeft,
+  fadeOutRight,
+  fadeOutDownBig,
+  fadeOutUpBig,
+  fadeOutLeftBig,
+  fadeOutRightBig
+} from './fading-exits';
+import {
+  flipInX,
+  flipInY,
+  flipOutX,
+  flipOutY
+} from './flippers';
+import {
+  lightSpeedIn,
+  lightSpeedOut
+} from './lightspeed';
+import {
+  slideInDown,
+  slideInUp,
+  slideInLeft,
+  slideInRight
+} from './sliding-entrances';
+import {
+  slideOutDown,
+  slideOutUp,
+  slideOutLeft,
+  slideOutRight
+} from './sliding-exits';
+import {
+  zoomIn,
+  zoomInDown,
+  zoomInUp,
+  zoomInLeft,
+  zoomInRight
+} from './zooming-entrances';
+import {
+  zoomOut,
+  zoomOutDown,
+  zoomOutUp,
+  zoomOutLeft,
+  zoomOutRight
+} from './zooming-exits';
+
+export {
+  bounce,
+  flash,
+  jello,
+  pulse,
+  rotate,
+  shake,
+  swing,
+  rubberBand,
+  tada,
+  wobble,
+  bounceIn,
+  bounceInUp,
+  bounceInDown,
+  bounceInRight,
+  bounceInLeft,
+  bounceOut,
+  bounceOutUp,
+  bounceOutDown,
+  bounceOutRight,
+  bounceOutLeft,
+  fadeIn,
+  fadeInDown,
+  fadeInUp,
+  fadeInLeft,
+  fadeInRight,
+  fadeInDownBig,
+  fadeInUpBig,
+  fadeInLeftBig,
+  fadeInRightBig,
+  fadeOut,
+  fadeOutDown,
+  fadeOutUp,
+  fadeOutLeft,
+  fadeOutRight,
+  fadeOutDownBig,
+  fadeOutUpBig,
+  fadeOutLeftBig,
+  fadeOutRightBig,
+  flipInX,
+  flipInY,
+  flipOutX,
+  flipOutY,
+  lightSpeedIn,
+  lightSpeedOut,
+  slideInDown,
+  slideInUp,
+  slideInLeft,
+  slideInRight,
+  slideOutDown,
+  slideOutUp,
+  slideOutLeft,
+  slideOutRight,
+  zoomIn,
+  zoomInDown,
+  zoomInUp,
+  zoomInLeft,
+  zoomInRight,
+  zoomOut,
+  zoomOutDown,
+  zoomOutUp,
+  zoomOutLeft,
+  zoomOutRight
+};
